141832851f57ecdde674e4b3fd4ee9c378c208aa
Ben Norskov <norskovb@newschool.edu>
2018-05-25 23:38:45 -0400

    Updated Resources folder and switching code for Player 1
    I don't know what to put in descriptions I don't put in the commit message, lol

--- Assets/Scripts/P1Controller.cs
+++ Assets/Scripts/P1Controller.cs
@@ -101,19 +101,61 @@ public class P1Controller : MonoBehaviour {
     void activateObject(int index){
         /*
             [name] => [name]-p1-active
+
+            // Assigns a material named "Assets/Resources/DEV_Orange" to the object.
+            Material newMat = Resources.Load("DEV_Orange", typeof(Material)) as Material;
+            gameObject.renderer.material = newMat;
         */
+        //
+        string playerNumberString = "-p1-active";
+
+        //first, turn all objects inactive:
+        foreach(GameObject g in childObjects) {
+            Renderer rnd = g.GetComponent<Renderer>();
+            if (rnd != null) {
+                string currentMaterialName = g.GetComponent<Renderer>().material.name;
+                if (currentMaterialName.Contains(playerNumberString))  {
+                    currentMaterialName = currentMaterialName.Remove(currentMaterialName.Length - 11 - playerNumberString.Length);
+                    // remove 11 for " (instance)" and 10 for playerNumberString
+                    Material switchToThis = Resources.Load(currentMaterialName, typeof(Material)) as Material;
+                    if (switchToThis != null){
+                        // if loaded correctly, then change the material.
+                        // print("Going to load " + currentMaterialName);
+                        rnd.material = switchToThis;
+                    } else {
+                        Debug.LogError("P1Controller.activateObject did not load a new texture for naviation. Name of texture: " + currentMaterialName);
+                    }
+                }
+            }
+        }
+
         //check of there is a game object in the array corrisponding to a index given
         if(childObjects[index]!= null){
-           Material myMaterial = childObjects[index].GetComponent<Renderer>().material;
-            if (myMaterial !=null){
-                Debug.Log(childObjects[index] + " is now active");
-                childObjects[index].GetComponent<Renderer>().material = activeMaterial;
+            Renderer rnd = childObjects[index].GetComponent<Renderer>();
+            if (rnd !=null){
+                string currentMaterialName = rnd.material.name;
+                currentMaterialName = currentMaterialName.Remove(currentMaterialName.Length - 11); // remove " (Instance)" from the name (11 chars)
+                if (!currentMaterialName.Contains(playerNumberString)) {
+                    currentMaterialName = currentMaterialName + playerNumberString;
+                    print(currentMaterialName + " after add");
+
+                    // use that string to load the correct material:
+                    Material switchToThis = Resources.Load(currentMaterialName, typeof(Material)) as Material;
+                    if (switchToThis != null) {
+                        // if loaded correctly, then change the material.
+                        //print("Going to load " + currentMaterialName);
+                        rnd.material = switchToThis;
+                    } else {
+                        Debug.LogError("P1Controller.activateObject did not load a new texture for naviation. Name of texture: " + currentMaterialName);
+                    }
+                }
+                //Debug.Log(childObjects[index] + " is now active");
             }
         }
     }
     void selectObject() {
         /*
-         * Called when you're at an object that is selected. 
+         * Called when you're at an object that is active. 
          */
     }
 
